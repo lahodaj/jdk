d3491a60ce8b96dbe4fa6ee30f0b205236e848cf
Nizar Benalla <nizar.benalla@oracle.com>
2024-04-01 23:51:22 +0000

    dealing with modules with no unqualified exports

--- test/jdk/tools/lib/tests/SinceCheckerHelper.java
+++ test/jdk/tools/lib/tests/SinceCheckerHelper.java
@@ -49,53 +49,86 @@ public class SinceCheckerHelper {
     //TODO IIRC we will need a bit more of this when we include java.compiler and jdk.compiler. I think I would create
     // a separated section for detection of "isPreview", which would include this set (which may need to be changed to a map,
     // saying in which versions the given element was a preview).
+
+    //I believe an array will be better for small n
     private final Set<String> LEGACY_PREVIEW_METHODS = Set.of(
             //13
             "method:java.lang.String:stripIndent:()",
             "method:java.lang.String:translateEscapes:()",
             "method:java.lang.String:formatted:(java.lang.Object[])",
+            //14
             "method:com.sun.source.util.SimpleTreeVisitor:visitYield:(com.sun.source.tree.YieldTree,java.lang.Object)",
             "method:com.sun.source.util.TreeScanner:visitYield:(com.sun.source.tree.YieldTree,java.lang.Object)",
             "method:com.sun.source.tree.TreeVisitor:visitYield:(com.sun.source.tree.YieldTree,java.lang.Object)",
             "field:com.sun.source.tree.Tree.Kind:YIELD",
-            "interface:com.sun.source.tree.YieldTree"
-            //14
+            "interface:com.sun.source.tree.YieldTree",
+            "class:javax.lang.model.element.RecordComponentElement",
+            "method:javax.lang.model.element.ElementVisitor:visitRecordComponent:(javax.lang.model.element.RecordComponentElement,P)",
+            "class:javax.lang.model.util.ElementScanner14",
+            "class:javax.lang.model.util.AbstractElementVisitor14",
+            "class:javax.lang.model.util.SimpleElementVisitor14",
+            "class:javax.lang.model.util.ElementKindVisitor6",
+            "class:javax.lang.model.util.ElementKindVisitor14",
+            "method:javax.lang.model.util.Elements:recordComponentFor:(javax.lang.model.element.ExecutableElement)",
+            "method:javax.lang.model.util.ElementFilter:recordComponentsIn:(java.lang.Iterable)",
+            "method:javax.lang.model.util.ElementFilter:recordComponentsIn:(java.util.Set)",
+            "method:javax.lang.model.element.TypeElement:getRecordComponents:()",
+            "field:javax.lang.model.element.ElementKind:RECORD",
+            "field:javax.lang.model.element.ElementKind:RECORD_COMPONENT",
+            "field:javax.lang.model.element.ElementKind:BINDING_VARIABLE",
+            "field:com.sun.source.tree.Tree.Kind:RECORD",
+            //"field:sun.reflect.annotation.TypeAnnotation.TypeAnnotationTarget:RECORD_COMPONENT"
+            "class:java.lang.reflect.RecordComponent",
+            "class:java.lang.runtime.ObjectMethods",
+            "field:java.lang.annotation.ElementType:RECORD_COMPONENT",
+            "method:java.lang.Class:isRecord:()",
+            "method:java.lang.Class:getRecordComponents:()",
+            "class:java.lang.Record",
+            "field:jdk.jshell.Snippet.SubKind:RECORD_SUBKIND"
     );
+
 
     private static final String JDK13 = "13";
     private static final String JDK14 = "14";
     private final Map<String, IntroducedIn> classDictionary = new HashMap<>();
     private final JavaCompiler tool;
-
     private final List<String> wrongTagsList = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        SinceCheckerHelper sinceCheckerTestHelper = new SinceCheckerHelper();
-
+        SinceCheckerHelper sinceCheckerTestHelper = new SinceCheckerHelper(args[0]);
         if (args.length == 0) {
-            System.out.println("No module specified. Exiting...");
+            System.err.println("No module specified. Exiting...");
             System.exit(1);
         }
         sinceCheckerTestHelper.testThisModule(args[0]);
     }
 
-    private SinceCheckerHelper() throws Exception {
+    private SinceCheckerHelper(String moduleName) throws Exception {
         tool = ToolProvider.getSystemJavaCompiler();
         for (int i = 9; i <= Runtime.version().feature(); i++) {
+            //NOTE
+            //Modules such as java.smartcardio don't appear when using elements.getAllModuleElements() until jdk 11 even tho they existed before
+            //--add-module is necessary
             JavacTask ct = (JavacTask) tool.getTask(null, null, null,
-                    List.of("--release", String.valueOf(i)), null,
+                    List.of("--add-modules", moduleName, "--release", String.valueOf(i)), null,
                     Collections.singletonList(SimpleJavaFileObject.forSource(URI.create("myfo:/Test.java"), "")));
             ct.analyze();
             String version = String.valueOf(i);
             Elements elements = ct.getElements();
+            elements.getModuleElement("java.base"); // forces module graph to be instantiated
             elements.getAllModuleElements().forEach(me ->
                     processModuleRecord(me, version, ct));
         }
     }
 
     private void processModuleRecord(ModuleElement moduleElement, String releaseVersion, JavacTask ct) {
-        // handle exception here
+        //TODO handle exception here
         for (ModuleElement.ExportsDirective ed : ElementFilter.exportsIn(moduleElement.getDirectives())) {
+            classDictionary.computeIfAbsent(moduleElement.getQualifiedName().toString(), _ -> {
+                IntroducedIn introducedIn = new IntroducedIn();
+                introducedIn.introducedStable = releaseVersion;
+                return introducedIn;
+            });
             if (ed.getTargetModules() == null) {
                 analyzePackageRecord(ed.getPackage(), releaseVersion, ct);
             }
@@ -155,7 +188,6 @@ public class SinceCheckerHelper {
 
     private void testThisModule(String moduleName) throws Exception {
         List<Path> sources = new ArrayList<>();
-
 //        Path home = Paths.get(System.getProperty("java.home"));
 //        Path srcZip = home.resolve("lib").resolve("src.zip");
         Path srcZip = Path.of(pathToAPIKEY.pathToSRC);
@@ -207,20 +239,28 @@ public class SinceCheckerHelper {
                 Version packageTopVersion = getPackageTopVersion(packagePath, ed);
                 analyzePackageCheck(ed.getPackage(), ct, sources, packageTopVersion);
             } else {
-                Path moduleInfoFile = packagePath.resolve("module-info.java");
-                Version packageTopVersion = null;
-                if (Files.exists(moduleInfoFile)) {
-                    try {
-                        byte[] ModuleInfoAsBytes = Files.readAllBytes(moduleInfoFile);
-                        String ModuleInfoContent = new String(ModuleInfoAsBytes, StandardCharsets.UTF_8);
-                        packageTopVersion = getSinceFromPackage(ModuleInfoContent);
-                        ed.getPackage().getModifiers();
-                    } catch (IOException e) {
-                        System.err.println("module=info.java not found or couldn't be opened AND thos module has no unqualified exports");
-//                        throw new RuntimeException(e);
-                    }
-                }
+                checkerModuleVersion(moduleElement, packagePath);
             }
+        }
+    }
+
+    private void checkerModuleVersion(ModuleElement moduleElement, Path packagePath) {
+        Path moduleInfoFile = packagePath.resolve("module-info.java");
+        if (Files.exists(moduleInfoFile)) {
+            try {
+                byte[] ModuleInfoAsBytes = Files.readAllBytes(moduleInfoFile);
+                String ModuleInfoContent = new String(ModuleInfoAsBytes, StandardCharsets.UTF_8);
+                String moduleVersion = extractSinceVersionFromText(ModuleInfoContent).toString();
+                String moduleName = moduleElement.getQualifiedName().toString();
+                String introducedVersion = classDictionary.get(moduleName).introducedStable;
+                if (!moduleVersion.equals(introducedVersion)) {
+                    wrongTagsList.add("For  Module: " + moduleName
+                            + " Wrong since version " + moduleVersion + " instead of " + introducedVersion + "\n");
+                }
+            } catch (IOException e) {
+                System.err.println("module-info.java not found or couldn't be opened AND this module has no unqualified exports");
+            }
+
         }
     }
 
@@ -233,26 +273,20 @@ public class SinceCheckerHelper {
                 )
                 .resolve("package-info.java");
 
-
         Version packageTopVersion = null;
         if (Files.exists(pkgInfo)) {
             try {
                 byte[] packageAsBytes = Files.readAllBytes(pkgInfo);
                 String packageContent = new String(packageAsBytes, StandardCharsets.UTF_8);
-                packageTopVersion = getSinceFromPackage(packageContent);
-                ed.getPackage().getModifiers();
+                packageTopVersion = extractSinceVersionFromText(packageContent);
             } catch (IOException e) {
                 System.err.println("package-info.java not found or couldn't be opened");
-//            throw new RuntimeException(e);
             }
         }
 
-        return packageTopVersion != null ? packageTopVersion : null;
+        return packageTopVersion;
     }
 
-    private Version getSinceFromPackage(String packageContent) {
-        return extractSinceVersion(packageContent);
-    }
 
     private void analyzePackageCheck(PackageElement pe, JavacTask ct, List<Path> sources, Version packageTopVersion) {
         List<TypeElement> typeElements = ElementFilter.typesIn(pe.getEnclosedElements());
@@ -268,7 +302,8 @@ public class SinceCheckerHelper {
 
     private void analyzeClassCheck(TypeElement te, String version, JavadocHelper javadocHelper,
                                    Types types, Version enclosingVersion) {
-        if (!te.getModifiers().contains(Modifier.PUBLIC)) {
+        Set<Modifier> classModifiers = te.getModifiers();
+        if (!classModifiers.contains(Modifier.PUBLIC)) {
             return;
         }
         Version currentVersion = checkElement(te, te, types, javadocHelper, version, enclosingVersion);
@@ -293,7 +328,7 @@ public class SinceCheckerHelper {
         } catch (IOException e) {
             throw new RuntimeException("JavadocHelper failed for " + element);
         }
-        Version sinceVersion = comment != null ? extractSinceVersion(comment) : null;
+        Version sinceVersion = comment != null ? extractSinceVersionFromText(comment) : null;
         if (sinceVersion == null ||
                 (enclosingVersion != null && enclosingVersion.compareTo(sinceVersion) > 0)) {
             sinceVersion = enclosingVersion;
@@ -313,7 +348,7 @@ public class SinceCheckerHelper {
     }
 
 
-    private static Version extractSinceVersion(String documentation) {
+    private static Version extractSinceVersionFromText(String documentation) {
         Pattern pattern = Pattern.compile("@since\\s+(\\d+(?:\\.\\d+)?)");
         Matcher matcher = pattern.matcher(documentation);
         if (matcher.find()) {
@@ -357,11 +392,11 @@ public class SinceCheckerHelper {
     private String getElementName(TypeElement te, Element element, Types types) {
         String prefix = "";
         String suffix = "";
-
-        if (element.getKind().isField()) {
+        ElementKind kind = element.getKind();
+        if (kind.isField()) {
             prefix = "field";
             suffix = ":" + te.getQualifiedName() + ":" + element.getSimpleName();
-        } else if (element.getKind() == ElementKind.METHOD || element.getKind() == ElementKind.CONSTRUCTOR) {
+        } else if (kind == ElementKind.METHOD || kind == ElementKind.CONSTRUCTOR) {
             prefix = "method";
             ExecutableElement executableElement = (ExecutableElement) element;
             String methodName = executableElement.getSimpleName().toString();
@@ -369,14 +404,13 @@ public class SinceCheckerHelper {
                     .map(p -> types.erasure(p.asType()).toString())
                     .collect(Collectors.joining(",", "(", ")"));
             suffix = ":" + te.getQualifiedName() + ":" + methodName + ":" + descriptor;
-        } else if (element.getKind().isDeclaredType()) {
-            if (element.getKind().isClass()) {
+        } else if (kind.isDeclaredType()) {
+            if (kind.isClass()) {
                 prefix = "class";
-                suffix = ":" + te.getQualifiedName();
-            } else if (element.getKind().isInterface()) {
+            } else if (kind.isInterface()) {
                 prefix = "interface";
-                suffix = ":" + te.getQualifiedName();
             }
+            suffix = ":" + te.getQualifiedName();
         }
         return prefix + suffix;
     }
